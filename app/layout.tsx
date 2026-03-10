cde7c6343ae3c6c7cd91d8a2214d0624c3d630b0
shainadeguzman <91320777+shainadeguzman@users.noreply.github.com>
2026-03-10 15:04:21 +0800

    add header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-800`}
       >
         <NextTopLoader color="#0f172b" />
         {children}
---
+++ components/layout/Header.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 border-b bg-white/90 backdrop-blur">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        <Link href="/blog" className="text-xl font-semibold">
+          Next<span className="text-gray-900">Blog</span>
+        </Link>
+
+        <nav className="hidden md:flex items-center gap-8 text-sm">
+          <Link href="/blog" className="text-black font-medium">
+            Home
+          </Link>
+          <Link
+            href="blog"
+            className="text-gray-600 hover:text-black transition"
+          >
+            Blog
+          </Link>
+          <Link
+            href="/categories"
+            className="text-gray-600 hover:text-black transition"
+          >
+            Categories
+          </Link>
+          <Link
+            href="/about"
+            className="text-gray-600 hover:text-black transition"
+          >
+            About
+          </Link>
+        </nav>
+
+        <Link
+          href="/blog"
+          className="inline-flex items-center rounded-lg bg-black px-4 py-2 text-sm font-medium text-white hover:bg-gray-800 transition"
+        >
+          Explore Articles
+        </Link>
+      </div>
+    </header>
+  );
+}
