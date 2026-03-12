135462fc297d51f58a44f3f5201d2286e4ccd360
shainadeguzman <91320777+shainadeguzman@users.noreply.github.com>
2026-03-12 19:37:24 +0800

    connect Next.js to PostgreSQL

---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+import { Pool } from "pg";
+
+declare global {
+  var pgPool: Pool | undefined;
+}
+
+const pool =
+  global.pgPool ??
+  new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.pgPool = pool;
+}
+
+export default pool;
